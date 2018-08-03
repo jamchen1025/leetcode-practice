18b1236e9a3871a6e92c7861ac58f12477f1abd4
陳致諺 <jam.chen1025@gamil.com>
2018-08-03 21:32:28 +0800

    [fix] add description and solution thought

--- source-code/007-reverse-integer.cpp
+++ source-code/007-reverse-integer.cpp
@@ -1,3 +1,27 @@
+/**********************************************************************************
+題目：
+https://leetcode.com/problems/reverse-integer/description/
+Given a 32-bit signed integer, reverse digits of an integer.
+Example 1:
+Input: 123
+Output: 321
+Example 2:
+Input: -123
+Output: -321
+Example 3:
+Input: 120
+Output: 21
+Note:
+Assume we are dealing with an environment which could only store integers 
+within the 32-bit signed integer range: [−231,  231 − 1]. 
+For the purpose of this problem, assume that your function returns 0 
+when the reversed integer overflows.
+**********************************************************************************
+想法：
+正常的mod和除法即可
+注意要考慮怎麼知道sum超出範圍的情況
+可以用unsign int 擴大範圍
+**********************************************************************************/
 #include <iostream>
 #include <vector>
 #include <string>
@@ -36,6 +60,10 @@ int main() {
   cout << s.reverse(-123) << endl;  // Output: -321
   cout << s.reverse(123) << endl;   // Output: 21
   cout << s.reverse(0) << endl;     // Output: 0
-  cout << s.reverse(0) << endl;     // Output: 0
+  cout << s.reverse(1534236469) << endl;      // Output: 0
+  cout << s.reverse(-1534236469) << endl;     // Output: 0
+  cout << s.reverse(-2147483648) << endl;     // Output: 0
+  cout << s.reverse(1463847412) << endl;      // Output: 2147483641
+    
   return 0;
 }
